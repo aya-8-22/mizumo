5ea022bcab1c395f854fdd29ee5773e82ddeec52
aya-8-22 <7momo.no.tane74@gmail.com>
2026-05-04 01:19:53 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+# config/routes.rb
+# Rails アプリケーションのルーティング（URL とコントローラーの紐付け）を設定するファイル
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  # 【追加】ルート URL（ / ）にアクセスしたときに home コントローラーの index アクションを実行
+  root 'home#index'
+
+  # 【追加】/home/index にアクセスしたときに home コントローラーの index アクションを実行
+  get 'home/index'
 end
