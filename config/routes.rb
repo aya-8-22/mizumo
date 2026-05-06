15f40d4468291f88e370d2360dd1a748e41f5870
aya-8-22 <7momo.no.tane74@gmail.com>
2026-05-06 17:36:53 +0900

    issue #8: Userモデルの作成（weightカラムのみ）

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 # config/routes.rb
 # Rails アプリケーションのルーティング（URL とコントローラーの紐付け）を設定するファイル
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
