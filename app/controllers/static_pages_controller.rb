4a0d9d8d48343fc3e90cd061cf9c9dcc11117a09
aya-8-22 <7momo.no.tane74@gmail.com>
2026-05-21 05:05:51 +0900

    フェーズ2: 重要な機能修正を完了
    - ログイン後のリダイレクトを実装
    - 設定ページのバリデーションを実装
    - フラッシュメッセージの表示制御を実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
-# 【追加】app/controllers/static_pages_controller.rb
+# app/controllers/static_pages_controller.rb
 # 静的ページ（トップページなど）を管理するコントローラー
 class StaticPagesController < ApplicationController
-  # 【追加】トップページだけ認証をスキップ
+  # トップページだけ認証をスキップ
   skip_before_action :authenticate_user!, only: [:top]
+
   # トップページを表示するアクション
-  def top; end
+  def top
+    # 【修正】ログイン状態の場合、記録ページにリダイレクト
+    return unless user_signed_in?
+
+    redirect_to water_intakes_path
+  end
 end
