b76e413d2b90c0da7ef971fdda1b6cc97c1d49fc
aya-8-22 <7momo.no.tane74@gmail.com>
2026-09-07 04:05:17 +0900

    feat:独自ドメイン追加

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,10 +3,8 @@
 # app/mailers/application_mailer.rb
 # すべてのメーラーの基底クラス
 class ApplicationMailer < ActionMailer::Base
-  # 【修正】メールの送信元アドレスをテスト用に設定
-  # Resend のテスト用ドメインから送信（独自ドメイン不要）
-  # 【注意】独自ドメイン設定後、変更が必要
-  default from: 'onboarding@resend.dev'
+  # 【修正】独自ドメインの認証が完了したため送信元アドレスを本番用に変更
+  default from: 'info@mizumo-app.com'
 
   # メールのレイアウトを指定
   layout 'mailer'
